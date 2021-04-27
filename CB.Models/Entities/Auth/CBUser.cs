49997f229039a13dd9b701112f23c9e4fa2122ab
Karam Alktnani <ka_1999@outlook.sa>
2021-04-27 22:34:00 +0300

    Add user Table

--- CB.Models/Entities/Auth/CBUser.cs
+++ CB.Models/Entities/Auth/CBUser.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using CB.Models.Enums;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CB.Models.Entities.Auth
 {
-   public class CBUser : IdentityUser
+    public class CBUser : IdentityUser
     {
+        [Required]
+        public string FullName { get; set; }
+        public string FCMToken { get; set; }
+        public string AccessToken { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public bool IsDelete { get; set; }
+        public bool IsActive { get; set; }
+        public UserType UserType { get; set; }
     }
 }
---
+++ CB.Models/Enums/UserType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CB.Models.Enums
+{
+    public enum UserType
+    {
+        Admin = 1,
+        Supervisor = 2,
+        Client = 3
+    }
+}
